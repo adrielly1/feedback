b438355845758876b9fa7c2f97b433af75f77f40
adrielly1 <adriiellycamargo@gmail.com>
2024-06-10 04:01:15 +0000

    feat: instalação e implantação do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackground: "#131518",
+    cardGradient: "radial-gradient{98.96% 98.96% at 50% 0%, #232a34 0%, #181e27 100%}",
+    textColor: "#969fad",
+    submitButtonColor:'#fc7614',
+    white: "#fff",
+    feedbackButtonColor: "#262e38",
+    feedbackButtonText: "#7c8798",
+}
